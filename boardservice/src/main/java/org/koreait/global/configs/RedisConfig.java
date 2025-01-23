4164ff5d0eec46efa5f6030685778dbcd394f5ea
JINJU PARK <aricclunj@naver.com>
2025-01-24 00:11:45 +0900

    .

--- boardservice/src/main/java/org/koreait/global/configs/RedisConfig.java
+++ boardservice/src/main/java/org/koreait/global/configs/RedisConfig.java
@@ -10,6 +10,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig { // 레디스형태 추가! / 임시데이터, 기록하기 위한거 그래서 만료시간을 넣어준거
+    // 비회원을 구별하기 위해서 레디스를 추가한거 / ip라던지 // 로컬호스트(pc기준)을 구별하기 위해서. // 인증을 거치기위해서
     @Value("${spring.data.redis.host}")
     private String host;
 
